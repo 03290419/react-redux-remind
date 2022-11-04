58fc4624fcacf09fc48e3f8e4a459e440378bb36
pguswls <alfma123@naver.com>
2022-11-04 13:42:13 +0900

    refactor connect row level

--- src/containers/CounterContainer.jsx
+++ src/containers/CounterContainer.jsx
@@ -8,19 +8,19 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => ({
+    increase: () => {
+      dispatch(increase());
+    },
+    decrease: () => {
+      dispatch(decrease());
+    },
+  }),
+)(CounterContainer);
 // presentationer container 를 감싸서 export
 // mapStateToProps 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
 // mapDispatchToProps 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
