7d6973ee9cbf71413dbd0305adc3ef32f58514c4
hyunjin <alfma123@naver.com>
2022-11-05 18:22:15 +0900

    :recycle: counter reducer, action creator to createAction, handleActions
    counter reducer, action creator to createAction, handleActions

--- README.md
+++ README.md
@@ -91,3 +91,32 @@ unsubscribe(); // 추후 구독을 비활성화할 때 함수를 호출
   - 파라미터 외의 값에는 의존하면 안 된다.
   - 이전 상태는 절대로 건드리지 않고, 변화를 줄 새로운 상태 객체를 만들어서 반환한다.
   - 똑같은 파라미터로 호출된 리듀서 함수는 언제나 똑같은 결과 값을 반환해야 한다.
+
+# Redux-actions
+
+redux actions를 사용하면 액션 생성 함수를 더 짧은 코드로 작성할 수 있다. 리듀서를 작성할 때도 switch/case 문이 아닌 handleActions라는 함수를 사용하여 각 액션마다 업데이트 함수를 설정하는 형식으로 작성해 줄 수 있다.
+`yarn add redux-actions`
+
+- createAction
+
+```js
+export const increase = () => createAction(INCREASE); // 액션 생성 함수
+export const decrease = () => createAction(DECREASE); // 액션 생성 함수
+```
+
+createAction 을 사용하면 매번 객체를 직접 만들어 줄 필요 없이 더욱 간단하게 액션 생성 함수를 선언할 수 있다.
+
+- handleActions
+
+```js
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+```
+
+handleActions로 reducer를 간략하게 작성할 수 있다.
+함수의 첫 번째 파라미터에는 각 액션에 대한 업데이트 함수를 넣어주고, 두 번째 파라미터에는 초기 상태를 넣어준다.
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,24 +1,23 @@
+import { createAction, handleActions } from 'redux-actions';
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 // 모듈이름/액션의 형태 -> 중복방지용
 
-export const increase = () => ({ type: INCREASE }); // 액션 생성 함수
-export const decrease = () => ({ type: DECREASE }); // 액션 생성 함수
+export const increase = () => createAction(INCREASE); // 액션 생성 함수
+export const decrease = () => createAction(DECREASE); // 액션 생성 함수
 
 const initialState = {
   number: 0,
 };
 // 초기값
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return { number: state.number + 1 };
-    case DECREASE:
-      return { number: state.number - 1 };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
 // 리듀서
 export default counter;
